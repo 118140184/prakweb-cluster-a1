818bdba00d85d51b3393d135cc4e74f3058fa0a9
118140184 Alfha RIdho Akira <60598672+118140184@users.noreply.github.com>
2020-11-22 16:49:55 +0700

    Add files via upload

---
+++ A1-118140184-Minggu 2/PHP Dasar 118140184/latihan1.php
@@ -0,0 +1,18 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>118140184-A1-Latihan 1</title>
+</head>
+<body>
+    <h1>KALKULATOR</h1>
+    
+    <form action = "kalkulator.php" method="get">
+    Bilangan 1 : <input type ="text" Name="bilangan1"> 
+    <br><br>
+    Bilangan 2 : <input type ="text" Name="bilangan2">
+    <br><br>
+    <input type='submit' name='tombol' value='Hasil' href = "hasil.php">
+    </form>
+</body>
+</html>
---
+++ A1-118140184-Minggu 2/PHP Dasar 118140184/latihan2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>118140184-A1-Latihan 2</title>
+</head>
+<body>
+    <?php
+    $name=array("lanirne","aduh","qiffuat","toda", "anevi","samid","kifuat");
+    sort ($name);
+
+    for ($i= "0";$i<= "6";$i++){
+        echo"<b>";
+        echo$name[$i];
+        echo "</b>";
+	echo " ";
+    }
+    ?>
+</body>
+</html
